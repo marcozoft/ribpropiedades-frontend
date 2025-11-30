c2b26765871f80f702b90fe5a2385a49f6dc8b57
Marcos <marcozoft@gmail.com>
2025-11-30 20:30:18 -0300

    Cambia el video de presentación de la web a un iframe de YouTube

--- src/components/home-secciones/Video.tsx
+++ src/components/home-secciones/Video.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image"
 
-//1364 × 768
-// TODO: Acomodar size
+
 export const Video = () => {
   return (
     <div className="w-full">
       {/* <div className="max-w-7xl mx-auto px-4"> */}
-        <video 
-          className="w-full"
-          autoPlay muted playsInline loop
-          preload="none">
-          <source src="/videos/rib-home.mp4" type="video/mp4"/>
-          Tu navegador no soporta videos.
-        </video>    
+        <div className="relative w-full aspect-video md:-mt-[90px] md:pt-[90px] bg-black overflow-hidden group pointer-events-none">
+          <iframe
+            className="absolute inset-0 w-full h-full scale-110"
+            src="https://www.youtube.com/embed/IxfRAd0QlRU?autoplay=1&mute=1&controls=0&loop=1&playlist=IxfRAd0QlRU&modestbranding=1&rel=0&fs=0&showinfo=0"
+            title="RIB Propiedades - Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{ pointerEvents: 'none' }}
+          ></iframe>
+        </div>
       {/* </div> */}
     </div>
   )
