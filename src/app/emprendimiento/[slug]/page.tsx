bbca90cec966652b997371740c3fe6f70aa90dae
Martin Sauczuk <martin.sauczuk@gmail.com>
2025-12-15 23:13:44 -0300

    Incremental

--- src/app/emprendimiento/[slug]/page.tsx
+++ src/app/emprendimiento/[slug]/page.tsx
@@ -1,12 +1,17 @@
 import { EmprendimientoFullPage } from "@/src/components";
 import { getEmprendimientoById } from "@/src/requests";
 import { extractIdFromSlug } from "@/src/utils";
+import { notFound } from "next/navigation";
 
 
-export default async function Page(props: PageProps<'/propiedad/[slug]'>) {
+export default async function Page(props: PageProps<'/emprendimiento/[slug]'>) {
 
     const { slug } = await props.params;
-    const emprendimientoResponse = await getEmprendimientoById( extractIdFromSlug(slug) );    
+    const emprendimientoResponse = await getEmprendimientoById( extractIdFromSlug(slug) ).then(resp => {
+      return resp.status == 200
+          ? resp.json()
+          : notFound()
+    })
 
     return (
       <EmprendimientoFullPage emprendimientoResponse={ emprendimientoResponse } />
--- src/requests/getEmprendimientoById.ts
+++ src/requests/getEmprendimientoById.ts
@@ -1,13 +1,13 @@
 import { API_KEY, API_URL } from "../constants/constants";
 import { EmprendimientoIdResponse } from "../interfaces";
 
-export const getEmprendimientoById = async (id: number): Promise<EmprendimientoIdResponse> => {
+export const getEmprendimientoById = (id: number): Promise<Response> => {
 
    return fetch(`${API_URL}/emprendimientos/${id}`, {
       headers: {
          'X-API-Key': API_KEY
       },
       cache: 'force-cache'
-   }).then(resp => resp.json())
+   });
 
 }
