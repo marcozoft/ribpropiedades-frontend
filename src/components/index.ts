6269da7f50277a5580c412be32f6decd96602cb8
Martin Sauczuk <martin.sauczuk@gmail.com>
2026-01-03 01:46:53 -0300

    Organize imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,41 +1,37 @@
-export { IndicadorPrecio } from "./cards/cards-components";
-
-// UI components
-export { Navbar} from "./navbar/Navbar";
-export { Footer } from "./footer/Footer";
-
+// Skeletons (loaders)
+export { SkeletonCard } from "./skeletons/SkeletonCard";
+export { SkeletonFilterBar } from "./skeletons/SkeletonFilterBar";
 
 // Propiedad card
 export { PropiedadCard } from "./cards/PropiedadCard";
-
-
 export { IndicadorNumerico } from "./cards/cards-components";
+export { IndicadorPrecio } from "./cards/cards-components";
+
+// Emprendimientos card
+export { EmprendimientoCard } from "./cards/EmprendimientoCard";
 
 // Detalle full page
 export { PropiedadFullPage } from "./detalle-full-page/PropiedadFullPage";
+export { EmprendimientoFullPage } from "./detalle-full-page/EmprendimientoFullPage";
 
 // Carousel
 export { CarouselLanzamientos } from './carousel-lanzamientos/CarouselLanzamientos';
-
 export { CarouselFullPage } from "./detalle-full-page/detalle-full-page-components/CarouselFullPageClient";
-
 export { CarouselCards } from "./carousel-cards-propiedades/CarouselCards";
 
-export { EmprendimientoCard } from "./cards/EmprendimientoCard";
-
+// Componentes reutilizables
 export { YouTubeVideoCard } from "./detalle-full-page/detalle-full-page-components";
-
-export { EmprendimientoFullPage } from "./detalle-full-page/EmprendimientoFullPage";
-
+export { Navbar} from "./navbar/Navbar";
+export { Footer } from "./footer/Footer";
 
 // Buscador
 export { FiltersBar } from "./filters/FiltersBar";
+export { TituloDescriptivo } from "./filters/TituloDescriptivo";
 
-
+// Servicios
 export { LinkButton } from "./ui/buttons/LinkButton";
 export { ServicioCard } from "./ui/cards/ServicioCard";
 export { SocialNetworks } from "./ui/SocialNetworks";
-
 
 // Shadcn custom components
 export { Command, CommandDialog} from "./shadcn-components";
