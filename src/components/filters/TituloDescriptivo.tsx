3db9a3a6dfc79410c49efcd23ed7762d49fbd5c4
Martin Sauczuk <martin.sauczuk@gmail.com>
2026-01-17 21:54:26 -0300

    #77 done

--- src/components/filters/TituloDescriptivo.tsx
+++ src/components/filters/TituloDescriptivo.tsx
@@ -32,7 +32,7 @@ export const TituloDescriptivo = ({
 
    return (
       <div className="max-w-6xl mx-auto flex flex-col justify-between pb-2 min-h-40">
-         <h1 className="text-5xl text-black font-bold">
+         <h1 className="text-3xl text-black font-bold">
             {length} {length === 1 ? 'propiedad' : 'propiedades'} {length === 1 ? 'encontrada' : 'encontradas'}
          </h1>
          <br />
