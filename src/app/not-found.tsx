2f62fdde9ad1d05856763254ab764a127766574a
Martin Sauczuk <martin.sauczuk@gmail.com>
2026-02-11 19:30:46 -0300

    Not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function NotFound() {
 
   return (
-      <div className="relative w-full h-[560px]">
+      <div className="relative w-full h-140">
         <Image className='absolute inset-0 object-cover w-full h-full' 
           src={'/images/home-tu-proyecto-realidad.png'} 
           width={1920} 
@@ -14,7 +14,7 @@ export default function NotFound() {
 
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
           <h1 className="text-white text-2xl font-bold mb-6">No encontramos esta propiedad, pero podemos ayudarte a encontrar la casa de tus sueños</h1>
-          <LinkButton className={'bg-white text-foreground rounded-full font-semibold text-xl'} href={'contacto'} text="CONTACTANOS" />
+          <LinkButton className={'bg-white text-foreground rounded-full font-semibold text-xl'} href={'propiedades'} text="SEGUIR BUSCANDO" />
         </div>
       </div>
   );
