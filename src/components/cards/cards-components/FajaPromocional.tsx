6ba774923b7d1131822f3c4681aa170ca3842bb3
Martin Sauczuk <martin.sauczuk@gmail.com>
2026-02-21 22:32:36 -0300

    #116 resuelto

--- src/components/cards/cards-components/FajaPromocional.tsx
+++ src/components/cards/cards-components/FajaPromocional.tsx
@@ -2,9 +2,9 @@ type FajaPromocionalProps = {
    descripcion: string
 }
 
-export const FajaPromocional = ({descripcion}: FajaPromocionalProps) => {
+export const FajaPromocional = ({ descripcion }: FajaPromocionalProps) => {
    return (
-      <span className="hidden sm:absolute sm:inline-flex top-0 right-2 z-2 mt-3 ml-3  select-none bg-foreground px-2 py-1 text-xs uppercase text-white">
+      <span className="absolute inline-flex top-0 right-2 z-2 mt-3 ml-3 select-none bg-foreground px-2 py-1 text-xs uppercase text-white">
          {descripcion}
       </span>
    )
