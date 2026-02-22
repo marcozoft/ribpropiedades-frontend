27cc51c4536f6f9fedb26da4463128c79fbeb11f
Martin Sauczuk <martin.sauczuk@gmail.com>
2026-02-22 18:48:32 -0300

    #117 Codigo de propiedad

--- src/components/cards/PropiedadCard.tsx
+++ src/components/cards/PropiedadCard.tsx
@@ -1,100 +1,119 @@
-import { PropiedadBasico } from '@/src/interfaces'
-import Image from 'next/image'
-import { FajaPromocional, IndicadorNumerico, IndicadorOperacion, IndicadorPrecio, TituloDeVenta } from './cards-components';
-import Link from 'next/link';
-import { generateHrefPropiedad, generateSrcImage } from '@/src/utils';
+import { PropiedadBasico } from "@/src/interfaces";
+import Image from "next/image";
+import {
+  FajaPromocional,
+  IndicadorNumerico,
+  IndicadorOperacion,
+  IndicadorPrecio,
+  TituloDeVenta,
+} from "./cards-components";
+import Link from "next/link";
+import { generateHrefPropiedad, generateSrcImage } from "@/src/utils";
 
 type Props = {
-   propiedad: PropiedadBasico
-   className?: string;
-}
+  propiedad: PropiedadBasico;
+  className?: string;
+};
 
 export const PropiedadCard = ({ propiedad, className }: Props) => {
+  const {
+    codigo,
+    dormitorios,
+    faja_promocional,
+    id,
+    imagen_principal,
+    inmueble,
+    operacion,
+    precio_publico,
+    precio,
+    sup_total,
+    sup_terreno,
+    titulo_venta,
+    zona,
+  } = propiedad;
 
-   const {
-      codigo,
-      dormitorios,
-      faja_promocional,
-      id,
-      imagen_principal,
-      inmueble,
-      operacion,
-      precio_publico,
-      precio,
-      sup_total,
-      sup_terreno,
-      titulo_venta,
-      zona 
-   } = propiedad;
-
-   return (
-      <div className={`relative mx-auto w-full ${className}`}>
-         <Link href={generateHrefPropiedad(id, titulo_venta)} className="relative inline-block w-full">
-            <div className="bg-white p-4 shadow rounded-xs">
-               <div className="relative flex justify-center overflow-hidden rounded-xs">
-
-                  {/* Imagen */}
-                  <div className="w-full aspect-video transform transition-transform duration-500 ease-in-out hover:scale-110">
-                     <Image className="w-full h-full object-cover" src={generateSrcImage(imagen_principal)} width={950} height={534} alt={codigo} />
-                  </div>
-
-                  {/* Faja promocional */}
-                  {
-                     faja_promocional && <FajaPromocional descripcion={faja_promocional} />
-                  }
-               </div>
-
-               <div className="mt-4">
-                  
-                  {/* Tipo de operacion */}
-                  <IndicadorOperacion 
-                     tipoDeInmueble={inmueble} 
-                     tipoDeOperacion={operacion}
-                     zona={zona}
-                  />
-
-                  {/* Precio */}
-                  <IndicadorPrecio
-                     className='text-black' 
-                     precio={precio} 
-                     precio_condicion={precio_publico} 
-                     sinEspecificar='Consultar' 
-                     moneda='U$D'
-                  />
-               </div>
-               <div className="mt-2">
-                  {/* Descripcion */}
-                  <TituloDeVenta tituloVenta={titulo_venta} />
-               </div>
-               <div className="justify-center">
-                  <div className="mt-4 flex gap-2 overflow-hidden rounded-lg px-1 py-1 divide-x">
-
-                     {/* Cantidad de dormitorios */}
-                     <IndicadorNumerico 
-                        nombre='Dormitorios' 
-                        valor={dormitorios} 
-                        icono={<i className="flaticon-bed text-black"></i>}
-                     />
-
-                     {/* superficie total m2 */}
-                     <IndicadorNumerico 
-                        nombre='Sup. Total' 
-                        valor={sup_total} 
-                        icono={<i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>} 
-                        unidad='m²'
-                     />
-
-                     {/* superficie terreno m2 */}
-                     <IndicadorNumerico 
-                        nombre='Sup. Terreno' 
-                        valor={sup_terreno} 
-                        icono={<i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>} 
-                        unidad='m²'
-                     />
-                  </div>
-               </div>
+  return (
+    <div className={`relative mx-auto w-full ${className}`}>
+      <Link
+        href={generateHrefPropiedad(id, titulo_venta)}
+        className="relative inline-block w-full"
+      >
+        <div className="rounded-xs bg-white p-4 shadow">
+          <div className="relative flex justify-center overflow-hidden rounded-xs">
+            {/* Imagen */}
+            <div className="aspect-video w-full transform transition-transform duration-500 ease-in-out hover:scale-110">
+              <Image
+                className="h-full w-full object-cover"
+                src={generateSrcImage(imagen_principal)}
+                width={950}
+                height={534}
+                alt={codigo}
+              />
             </div>
-         </Link>
-      </div>
-   )
-}
+
+            {/* Faja promocional */}
+            {faja_promocional && (
+              <FajaPromocional descripcion={faja_promocional} />
+            )}
+          </div>
+
+          <div className="mt-1">
+            <p className="text-right text-xs text-black">Código: {codigo}</p>
+          </div>
+
+          <div className="mt-2">
+            {/* Tipo de operacion */}
+            <IndicadorOperacion
+              tipoDeInmueble={inmueble}
+              tipoDeOperacion={operacion}
+              zona={zona}
+            />
+
+            {/* Precio */}
+            <IndicadorPrecio
+              className="text-black"
+              precio={precio}
+              precio_condicion={precio_publico}
+              sinEspecificar="Consultar"
+              moneda="U$D"
+            />
+          </div>
+          <div className="mt-2">
+            {/* Descripcion */}
+            <TituloDeVenta tituloVenta={titulo_venta} />
+          </div>
+          <div className="justify-center">
+            <div className="mt-4 flex gap-2 divide-x overflow-hidden rounded-lg px-1 py-1">
+              {/* Cantidad de dormitorios */}
+              <IndicadorNumerico
+                nombre="Dormitorios"
+                valor={dormitorios}
+                icono={<i className="flaticon-bed text-black"></i>}
+              />
+
+              {/* superficie total m2 */}
+              <IndicadorNumerico
+                nombre="Sup. Total"
+                valor={sup_total}
+                icono={
+                  <i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>
+                }
+                unidad="m²"
+              />
+
+              {/* superficie terreno m2 */}
+              <IndicadorNumerico
+                nombre="Sup. Terreno"
+                valor={sup_terreno}
+                icono={
+                  <i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>
+                }
+                unidad="m²"
+              />
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
--- src/components/detalle-full-page/detalle-full-page-components/SeccionPropiedad.tsx
+++ src/components/detalle-full-page/detalle-full-page-components/SeccionPropiedad.tsx
@@ -1,14 +1,19 @@
 import { secondaryFont } from "@/src/config/fonts";
-import { PropiedadDetalleResponse } from "@/src/interfaces"
-import { DetallesGrid, YouTubeVideoCard } from '.';
-import { IndicadorNumerico, IndicadorPrecio, MapaPropiedad } from '@/src/components';
+import { PropiedadDetalleResponse } from "@/src/interfaces";
+import { DetallesGrid, YouTubeVideoCard } from ".";
+import {
+  IndicadorNumerico,
+  IndicadorPrecio,
+  MapaPropiedad,
+} from "@/src/components";
 
 type SeccionPropiedadProps = {
-  propiedadResponse: PropiedadDetalleResponse
-}
+  propiedadResponse: PropiedadDetalleResponse;
+};
 
-export const SeccionPropiedad = ({ propiedadResponse }: SeccionPropiedadProps) => {
-
+export const SeccionPropiedad = ({
+  propiedadResponse,
+}: SeccionPropiedadProps) => {
   const {
     titulo_venta,
     operacion,
@@ -21,91 +26,119 @@ export const SeccionPropiedad = ({ propiedadResponse }: SeccionPropiedadProps) =
     sup_total,
     sup_terreno,
     zona,
+    codigo,
   } = propiedadResponse.propiedad;
 
-
   return (
-    <section id="descripcion" className="scroll-mt-28" >
-
+    <section id="descripcion" className="scroll-mt-28">
       {/* Titulo, operacion, nombre del emprendimiento (si pertenece a un emprendimiento) y precio */}
-      <div className="flex justify-between mb-8">
-        <span className="bg-foreground text-white uppercase text-sm py-0.5 px-1.5">
-          {propiedadResponse.emprendimiento?.nombre ? `${propiedadResponse.emprendimiento.nombre} - ${operacion}` : operacion}
-        </span>
+      <div className="mb-8 flex justify-between">
+        <div className="justify-between">
+          <span className="bg-foreground px-1.5 py-0.5 text-sm text-white uppercase">
+            {propiedadResponse.emprendimiento?.nombre
+              ? `${propiedadResponse.emprendimiento.nombre} - ${operacion}`
+              : operacion}
+          </span>
+        </div>
+        <div className="flex items-center">
+          <p className="text-right text-xs text-black sm:text-sm">
+            Código: {codigo}
+          </p>
+        </div>
       </div>
-      <div className="flex justify-between mb-10 items-center">
-        <h1 className="font-semibold text-xl lg:text-3xl xl:text-4xl mt-4 text-black">{titulo_venta}</h1>
-        <IndicadorPrecio precio={precio} precio_condicion={precio_publico} moneda="U$D" sinEspecificar="Consultar" />
+
+      <div className="mb-10 flex items-center justify-between gap-2">
+        <h1 className="mt-4 text-xl font-semibold text-black lg:text-3xl xl:text-4xl">
+          {titulo_venta}
+        </h1>
+        <IndicadorPrecio
+          precio={precio}
+          precio_condicion={precio_publico}
+          moneda="U$D"
+          sinEspecificar="Consultar"
+        />
       </div>
 
       {/* Detalles enumerados */}
-      <h2 className="font-bold text-black text-xl mt-8 mb-4"><span className="text-foreground">|&nbsp;</span>Detalles</h2>
+      <h2 className="mt-8 mb-4 text-xl font-bold text-black">
+        <span className="text-foreground">|&nbsp;</span>Detalles
+      </h2>
 
       <div className="justify-center">
-        <div className="my-5 gap-2 flex overflow-hidden rounded-lg divide-x">
-
+        <div className="my-5 flex gap-2 divide-x overflow-hidden rounded-lg">
           {/* Zona/localidad */}
           <IndicadorNumerico
             nombre={zona}
             icono={<i className="flaticon-pin text-black"></i>}
-            unidad=''
+            unidad=""
           />
 
           {/* superficie total m2 */}
           <IndicadorNumerico
-            nombre='Sup. Total'
+            nombre="Sup. Total"
             valor={sup_total}
-            icono={<i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>}
-            unidad='m²'
+            icono={
+              <i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>
+            }
+            unidad="m²"
           />
           {/* superficie terreno m2 */}
           <IndicadorNumerico
-            nombre='Sup. Terreno'
+            nombre="Sup. Terreno"
             valor={sup_terreno}
-            icono={<i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>}
-            unidad='m²'
+            icono={
+              <i className="flaticon-square-shape-design-interface-tool-symbol text-black"></i>
+            }
+            unidad="m²"
           />
         </div>
       </div>
 
-
       <DetallesGrid
         propiedad={propiedadResponse.propiedad}
         detalles={[
-          { descripcion: 'Estilo', clave: 'estilo' },
-          { descripcion: 'Nro. de plantas', clave: 'plantas' },
-          { descripcion: 'Dormitorios', clave: 'dormitorios' },
-          { descripcion: 'Dormitorios en suite', clave: 'dormitorio_suite' },
-          { descripcion: 'Estado', clave: 'estado' },
-          { descripcion: 'Antiguedad', clave: 'antiguedad' },
-          { descripcion: 'Lote', clave: 'lote' },
-          { descripcion: 'Tipo de zona', clave: 'tipo_zona' },
-          { descripcion: 'Tipo de calefacción', clave: 'calefaccion' },
-          { descripcion: 'Aberturas', clave: 'aberturas' },
-        ]} />
+          { descripcion: "Estilo", clave: "estilo" },
+          { descripcion: "Nro. de plantas", clave: "plantas" },
+          { descripcion: "Dormitorios", clave: "dormitorios" },
+          { descripcion: "Dormitorios en suite", clave: "dormitorio_suite" },
+          { descripcion: "Estado", clave: "estado" },
+          { descripcion: "Antiguedad", clave: "antiguedad" },
+          { descripcion: "Lote", clave: "lote" },
+          { descripcion: "Tipo de zona", clave: "tipo_zona" },
+          { descripcion: "Tipo de calefacción", clave: "calefaccion" },
+          { descripcion: "Aberturas", clave: "aberturas" },
+        ]}
+      />
 
       {/* Descripcion */}
-      <h2 className="font-bold text-black text-xl mt-8 mb-4"><span className="text-foreground">|&nbsp;</span>Descripción</h2>
+      <h2 className="mt-8 mb-4 text-xl font-bold text-black">
+        <span className="text-foreground">|&nbsp;</span>Descripción
+      </h2>
 
       {/* Descripcion larga */}
-      <p className={`${secondaryFont.className} text-black text-lg`}>{descripcion_larga}</p>
+      <p className={`${secondaryFont.className} text-lg text-black`}>
+        {descripcion_larga}
+      </p>
 
       {/* Video (opcional) */}
-      {
-        video && (
-          <>
-            <h2 className="font-bold text-black text-xl my-8"><span className="text-foreground">|&nbsp;</span>Video</h2>
-            <YouTubeVideoCard youTubeId={video} />
-          </>
-        )
-      }
+      {video && (
+        <>
+          <h2 className="my-8 text-xl font-bold text-black">
+            <span className="text-foreground">|&nbsp;</span>Video
+          </h2>
+          <YouTubeVideoCard youTubeId={video} />
+        </>
+      )}
       {/* Map */}
-      <h2 className="font-bold text-black text-xl my-8"><span className="text-foreground">|&nbsp;</span>Ubicación</h2>
+      <h2 className="my-8 text-xl font-bold text-black">
+        <span className="text-foreground">|&nbsp;</span>Ubicación
+      </h2>
 
-      <MapaPropiedad latitud={+mapa_latitud} longitud={+mapa_longitud} className="h-80" />
-
+      <MapaPropiedad
+        latitud={+mapa_latitud}
+        longitud={+mapa_longitud}
+        className="h-80"
+      />
     </section>
-
-
-  )
-}
+  );
+};
