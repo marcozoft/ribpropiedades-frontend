aac2388a93bf6080c631c208a28816350898cd7d
Martin Sauczuk <martin.sauczuk@gmail.com>
2026-02-22 19:36:26 -0300

    #120 Boton Whatsapp siempre visible

--- src/components/ui/SocialNetworks.tsx
+++ src/components/ui/SocialNetworks.tsx
@@ -1,14 +1,45 @@
-import { FACEBOOK_LINK, INSTAGRAM_LINK, LINKEDIN_LINK, WHATSAPP_LINK, YOUTUBE_LINK } from "@/src/constants/constants"
-import { SocialNetworkItem } from "./SocialNetworkItem"
+import {
+  FACEBOOK_LINK,
+  INSTAGRAM_LINK,
+  LINKEDIN_LINK,
+  WHATSAPP_LINK,
+  YOUTUBE_LINK,
+} from "@/src/constants/constants";
+import { SocialNetworkItem } from "./SocialNetworkItem";
 
 export const SocialNetworks = () => {
   return (
-    <div className="hidden sm:flex gap-2">
-      <SocialNetworkItem src="/images/navbar-instagram.svg" href={INSTAGRAM_LINK} alt="instagram logo"/>
-      <SocialNetworkItem src="/images/navbar-facebook.svg"  href={FACEBOOK_LINK} alt="facebook logo"/>
-      <SocialNetworkItem src="/images/navbar-youtube.svg"   href={YOUTUBE_LINK} alt="youtube logo"/>
-      <SocialNetworkItem src="/images/navbar-linkedin.svg"  href={LINKEDIN_LINK} alt="linkedin logo"/>
-      <SocialNetworkItem src="/images/navbar-whatsapp.svg"  href={WHATSAPP_LINK} alt="whatsapp logo"/>
+    <div className="flex gap-2">
+      <SocialNetworkItem
+        className="hidden sm:flex"
+        src="/images/navbar-instagram.svg"
+        href={INSTAGRAM_LINK}
+        alt="instagram logo"
+      />
+      <SocialNetworkItem
+        className="hidden sm:flex"
+        src="/images/navbar-facebook.svg"
+        href={FACEBOOK_LINK}
+        alt="facebook logo"
+      />
+      <SocialNetworkItem
+        className="hidden sm:flex"
+        src="/images/navbar-youtube.svg"
+        href={YOUTUBE_LINK}
+        alt="youtube logo"
+      />
+      <SocialNetworkItem
+        className="hidden sm:flex"
+        src="/images/navbar-linkedin.svg"
+        href={LINKEDIN_LINK}
+        alt="linkedin logo"
+      />
+      <SocialNetworkItem
+        className="flex"
+        src="/images/navbar-whatsapp.svg"
+        href={WHATSAPP_LINK}
+        alt="whatsapp logo"
+      />
     </div>
-  )
-}
+  );
+};
